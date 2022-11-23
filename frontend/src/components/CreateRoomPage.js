cfa6c6bca8703333afeb379017fd2bd7191baf0c
SeaMan111 <agz97agz97@gmail.com>
2022-11-23 13:01:05 +0800

    updetes

---
+++ frontend/src/components/CreateRoomPage.js
@@ -0,0 +1,11 @@
+import react, {Component} from 'react';
+
+export default class CreateRoomPage extends Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <p>Here To Create Room Page</p>
+    }
+}
---
+++ frontend/src/components/RoomJoinPage.js
@@ -0,0 +1,11 @@
+import react, {Component} from 'react';
+
+export default class RoomJoinPage extends Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <p>This is the Room Join Page</p>
+    }
+}
